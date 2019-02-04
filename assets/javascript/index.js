f40a31086ddf7f83fbc28e3f53fd6ccb417c31b5
dPaq <dpaquette2.0@gmail.com>
2019-02-04 14:10:12 -0500

    finished

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
 
-
-
     /*
-        Initialize Firebase
-        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+        Initialize Firebase & Global Variables
+        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
     */
     var config = {
         apiKey: "AIzaSyBknjX2o3AQCT_E5xADzK9sH7QknFBxKpE",
@@ -19,17 +17,15 @@ $(document).ready(function(){
     var active = db.ref("/connections")
     var connected = db.ref(".info/connected")
 
-    /*
-        Global Variables
-        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-    */
-
+    //
+    //  Global Variables
+    //  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
     var player = {
         name: '',
         wins: 0,
         lost: 0,
         gameid: false,
-        id: Math.floor(Math.random()*Math.pow(10,16)),
+        id: Math.floor(Math.random()*Math.pow(10,15)),
     }
     var opp = {
         name: '',
@@ -37,15 +33,20 @@ $(document).ready(function(){
         lost: 0,
         id: false
     }
+    var gameNum
+
+
+
+
 
     /*
         Function Farm
-        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
     */
 
     //
-    //    Login functions
-    //
+    //  Login functions
+    //  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 
     // (1) adds user record to 'connections' & 'users' branches
     function recordOnline(α) {
@@ -80,62 +81,50 @@ $(document).ready(function(){
     // (3) find opponent
     function findOpponent(ε) {
         let online = [], players = {}
-        if (opp.id > 0) {
+        if (opp.id > 0) {   // exit if we already have an opponent
             return
+        }
+        for (let i in ε.val()) {
+            players[i] = ε.val()[i]
         }
         // get online player list (online)
         db.ref('connections').once('value',function(α){
-            // console.log(α.val())
             for (let i in α.val()) {
                 online.push(α.val()[i].id)
-                // if (α.val()[i].id !== player.id) {
-                //     opp.id = α.val()[i].id
-                // }
             }
-        }).then(
-            // get user list
-            db.ref('users').once('value',function(α){
-                for (let i in α.val()) {
-                    players[i] = α.val()[i]
-                }
-            }).then(function(){
-                // determine/find opponent
-                // If we have an opponent, and they are on the list, return - we con't care
-                // we also don't care if there is only one player online
-                if ((!opp.id && online.indexOf(opp.id) >= 0) || online.length === 1) {
-                    return
-                } else {
-                    if (players[player.id].available === false) {
+        }).then(function(){
+            // update display w/ win/lost numbers
+            // determine/find opponent
+            // If we have an opponent, and they are on the list, return - we con't care
+            // we also don't care if there is only one player online
+            if ((!opp.id && online.indexOf(opp.id) >= 0) || online.length === 1) {
+                return
+            } else {
+                if (players[player.id].available === false) {
                     // mark yourself available if you're marked as not available
                     db.ref('users').child(player.id).child('available').set(true)
-                    }
-                    // find an opponent
-                    for (let i in online) {
-                        // online[i] is an id
-                        if (online[i] !== player.id && players[online[i]].available === true) {
-                            // this is our new opponent
-                            opp.id = online[i]
-                            db.ref('users').child(opp.id).child('available').set(false)
-                            console.log('my id: ' + player.id)
-                            console.log("my opponent's id: " + opp.id)
-                        }
+                }
+                // find an opponent
+                for (let i in online) {
+                    // online[i] is an id
+                    if (online[i] !== player.id && players[online[i]].available === true) {
+                        // this is our new opponent
+                        gameNum = 1
+                        opp.id = online[i]
+                        db.ref('users').child(opp.id).child('available').set(false)
+                        let x = Math.min(player.id,opp.id)
+                        let y = Math.max(player.id,opp.id)
+                        player.gameid = x.toString() + y.toString() + gameNum.toString()
+                        db.ref('users').child(player.id).child('gameid').set(player.gameid)
                     }
                 }
-            })
-        )
-    }
-
-    function disconnect() {
-        db.ref('chat').push({
-            message: 'Disconnected',
-            time: firebase.database.ServerValue.TIMESTAMP,
+            }
         })
-
     }
 
     //
     //  Chat functions
-    //
+    //  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 
     // (1) sends chat message to db & resets field
     function sendChatMessage(ε) {
@@ -181,17 +170,141 @@ $(document).ready(function(){
     }
 
     //
-    //  Chat functions
-    //
+    //  Game functions
+    //  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
     function sendRps() {
         let choice = $(this).attr('id')
-        console.log(choice)
-        db.ref('game').child(player.id)
+        db.ref('games').once('value',function(x) {
+            try {
+                let id = player.id
+                let ζ = x.val()[player.gameid].id
+                let η = x.val()[player.gameid]
+                if (ζ || !η) {
+                    // if ζ exists, you've already picked this game
+                    // if η doesn't exist, you don't have an opponent
+                    // in either case we need to ignore this click
+                    return
+                }
+            } catch {
+                /*  nothing - this try{}catch{} section exists as an
+                    if there's an error, then abort, else continue
+                */
+            }
+        })
+        db.ref('games').child(player.gameid).child(player.id).set(choice)
     }
+
+    function playGame(α) {
+        if (!player.gameid) {
+            return
+        }
+        // won/lost function
+        function determineWinner(α,β) {
+            let result
+            if (
+                (α === 'rock' && β === 'scissors') ||
+                (α === 'scissors' && β === 'paper') ||
+                (α === 'paper' && β === 'rock')
+            ) {
+                result = 1
+            } else {
+                result = -1
+            }
+            if (α === β) {
+                result = 0
+            }
+            // if (α === 'rock') {
+            //     if (β === 'scissors') {
+            //         result = 1
+            //     } else {
+            //         result = -1
+            //     }
+            // }
+            // if (α === 'scissors') {
+            //     if (β === 'paper') {
+            //         result = 1
+            //     } else {
+            //         result = -1
+            //     }
+            // }
+            // if (α === 'paper') {
+            //     if (β === 'rock') {
+            //         result = 1
+            //     } else {
+            //         result = -1
+            //     }
+            // }
+            // if (α === β) {
+            //     result = 0
+            // }
+            return result
+        }
+        let game = α.val()[player.gameid]
+        let me = game[player.id]
+        let them = game[opp.id]
+        let outcome
+        if (!me && them) {
+            $('#ready-opponent').text('Picked & is waiting for you')
+        }
+        if (me) {
+            $('#ready-player').text('Picked ' + me)
+        }
+        if (me && them) {
+            // game is over, both have chosen, determine winner
+            outcome = determineWinner(me,them)
+            if (outcome > 0) {
+                // victory! - post the victory to chat
+                player.wins++
+                db.ref('users').child(player.id).child('won').set(player.wins)
+                db.ref('chat').push({
+                    name: player.name,
+                    message: me + ' beats ' + them + ", so I beat " + opp.name + '!',
+                    time: firebase.database.ServerValue.TIMESTAMP,
+                })
+                    }
+            if (outcome < 0) {
+                // defeat! - the opponent will post to chat
+                player.lost++
+                db.ref('users').child(player.id).child('lost').set(player.lost)
+            }
+            if (outcome === 0) {
+                // tie! - whoever's id is smaller will post the tie to chat
+                if (player.id > opp.id) {
+                    db.ref('chat').push({
+                        name: player.name,
+                        message: opp.name + ' and I both chose ' + me + ' and we tied!',
+                        time: firebase.database.ServerValue.TIMESTAMP,
+                    })
+                }
+            }
+            gameNum++
+            player.gameid = player.gameid.substring(0,30) + gameNum.toString()
+        }
+    }
+
+    function display(α) {
+        let users = {}
+        for (let i in α.val()) {
+            users[i] = α.val()[i]
+        }
+        $('#won-player').text('Won: ' + users[player.id].won)
+        $('#lost-player').text('Lost: ' + users[player.id].lost)
+        $('#name-player').text('You: ' + users[player.id].name)
+        if (opp.id) {
+            opp.name = users[opp.id].name
+            $('#name-opponent').text('Your Opponent: ' + opp.name)
+            $('#won-opponent').text('Won: ' + users[opp.id].won)
+            $('#lost-opponent').text('Lost: ' + users[opp.id].lost)
+        }
+    }
+
+
+
+
 
     /*
         Event Listeners
-        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
+        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
     */
 
     // login page listener
@@ -209,7 +322,14 @@ $(document).ready(function(){
     db.ref('chat').on('child_added',displayChatMessage)
 
     // user update listener - finds opponent
-    db.ref('users').on('child_added',findOpponent)
+    db.ref('users').on('value',findOpponent)
 
+    // user update listener #2 - display's opponent stats
+    db.ref('users').on('value',display)
+
+    // game play - send choice to database
     $(document).on('click','.btn-lg',sendRps)
+
+    // game play - listen for choice from opponent
+    db.ref('games').on('value',playGame)
 })
